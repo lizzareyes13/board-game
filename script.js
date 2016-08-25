c68274d09ef4c575804e8193b7656c1bf14b829c
Lizza Reyes <lizzareyes13@gmail.com>
2016-08-24 22:19:59 -0400

    players can now move on the board

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+
+var player1 = new Player($("#player1"))
+var player2 = new Player($("#player2"))
+
 // dice
+var whoseTurn = 1;
 $("#dado").click(function(){
   $("#dieroll").html("");
   $(this).attr("src", "Dodecahedron.gif");
   setTimeout(function(){
+    let num = Math.floor(Math.random()*12)+1;
     $("#dado").attr("src", "Dodecahedron.png");
-    $("#dieroll").html(Math.floor(Math.random()*12)+1);
+    $("#dieroll").html(num);
+    if (whoseTurn == 1){
 
+     player1.moveN(num);
+     whoseTurn = 2;
+   }
+   else{
+     whoseTurn = 1;
+     player2.moveN(num);
+   }
   },3100)
 })
 
@@ -36,4 +50,47 @@ for(let r = 0; r < 8; r++){
     tr.appendChild(td);
   }
 }
-document.getElementById("board-container").appendChild("tbl");
+document.getElementById("board-container").appendChild(tbl);
+
+// create players
+
+// Const Player1 = New player(){
+//   }
+
+
+function Player (e){
+  this.htmlElement = e;
+  this.pos = 0;
+  this.moveOne = function(){
+    var wantedid = "cell" + (this.pos+1);
+		var pos1 = $("#" + wantedid).offset(); //{top:__, left: __ }
+		this.htmlElement.css("top", pos1.top);
+		this.htmlElement.css("left", pos1.left);
+    this.pos = this.pos + 1;
+  }
+  this.moveN = function(n) {
+    let dest = this.pos + n;
+    const runMove = () => {
+      if (this.pos >= 27){
+        alert("Game Over");
+        return;
+      }
+      else if (this.pos >= dest) return;
+      else {
+        setTimeout (runMove, 500);
+        this.moveOne();
+      }
+    }
+    runMove();
+  }
+
+}
+//
+
+
+// move the players
+// Player1.moveOne("cell")
+
+// var finishLine = "cell" + 27{
+//   alert("winner");
+// }
